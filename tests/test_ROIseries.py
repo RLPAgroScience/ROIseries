f80e219a9f2af19ada66bd2e53476752f925a9ed
niklas_keck <niklas_keck@gmx.de>
2017-10-11 16:08:53 +0200

    Adjusted tests to work with new TAFtoTRF transformer implemented in 5d544b9dd53fa3979db8adf652f16e465edecaf5 - Additionally cleaned up the test CSV to be easier comprehensible: -- ID_1 has values from 10 to 19, ID_5 from 50 to 59 ID_7 from 70 to 71 -- removed one time step to fit into the 0-9 range

--- tests/test_ROIseries.py
+++ tests/test_ROIseries.py
@@ -12,17 +12,17 @@ import numpy as np
 # note: as DataFrames are mutable do not use scope="module" to prevent interactions between tests
 @pytest.fixture()
 def df():
-    df1 = pd.DataFrame([[311, 312, 313, 314, 315, 316], [71, 72, 73, 74, 75, 76], [351, 352, 353, 354, 355, 356]],
+    df1 = pd.DataFrame([[10, 11, 12, 13, 14], [70, 71, 72, 73, 74], [50, 51, 52, 53, 54]],
                        ["ID_1", "ID_7", "ID_5"],
                        ["Feature_1_2457350.0000000000", "Feature_1_2457362.0000000000",
                         "Feature_1_2457374.0000000000", "Feature_1_2457386.0000000000",
-                        "Feature_1_2457398.0000000000", "Feature_1_2457410.0000000000"])
+                        "Feature_1_2457398.0000000000"])
 
-    df2 = pd.DataFrame([[317, 318, 319, 320, 321, 322], [77, 78, 79, 80, 81, 82], [357, 358, 359, 360, 361, 362]],
+    df2 = pd.DataFrame([[15, 16, 17, 18, 19], [75, 76, 77, 78, 79], [55, 56, 57, 58, 59]],
                        ["ID_1", "ID_7", "ID_5"],
                        ["Feature_2_2457350.0000000000", "Feature_2_2457362.0000000000",
                         "Feature_2_2457374.0000000000", "Feature_2_2457386.0000000000",
-                        "Feature_2_2457398.0000000000", "Feature_2_2457410.0000000000"])
+                        "Feature_2_2457398.0000000000"])
 
     df = pd.concat([df1, df2], axis=1)
     df.index.names = ['ID']
@@ -42,12 +42,11 @@ def df_trf():
     csv = """,ID_1,ID_1,ID_1,ID_1,ID_1,ID_1,ID_7,ID_7,ID_7,ID_7,ID_7,ID_7,ID_5,ID_5,ID_5,ID_5,ID_5,ID_5
 ,Feature_1,Feature_1,Feature_1,Feature_2,Feature_2,Feature_2,Feature_1,Feature_1,Feature_1,Feature_2,Feature_2,Feature_2,Feature_1,Feature_1,Feature_1,Feature_2,Feature_2,Feature_2
 ,m1,m2,p1,m1,m2,p1,m1,m2,p1,m1,m2,p1,m1,m2,p1,m1,m2,p1
-2015-11-23,311,NA,312,317,NA,318,71,NA,72,77,NA,78,351,NA,352,357,NA,358
-2015-12-05,312,311,313,318,317,319,72,71,73,78,77,79,352,351,353,358,357,359
-2015-12-17,313,312,314,319,318,320,73,72,74,79,78,80,353,352,354,359,358,360
-2015-12-29,314,313,315,320,319,321,74,73,75,80,79,81,354,353,355,360,359,361
-2016-01-10,315,314,316,321,320,322,75,74,76,81,80,82,355,354,356,361,360,362
-2016-01-22,316,315,NA,322,321,NA,76,75,NA,82,81,NA,356,355,NA,362,361,NA
+2015-11-23,10,NA,11,15,NA,16,70,NA,71,75,NA,76,50,NA,51,55,NA,56
+2015-12-05,11,10,12,16,15,17,71,70,72,76,75,77,51,50,52,56,55,57
+2015-12-17,12,11,13,17,16,18,72,71,73,77,76,78,52,51,53,57,56,58
+2015-12-29,13,12,14,18,17,19,73,72,74,78,77,79,53,52,54,58,57,59
+2016-01-10,14,13,NA,19,18,NA,74,73,NA,79,78,NA,54,53,NA,59,58,NA
 """
 
     df_trf = pd.read_csv(io.StringIO(csv), index_col=[0], header=[0, 1, 2])
@@ -102,14 +101,14 @@ def test_reltime_from_absdate_freq(time):
 
 def test_reltime_from_absdate_reltime(time):
     reltime, freq = rs.feature_transformers.reltime_from_absdate(time)
-    assert reltime.equals(pd.Index([0.0, 1.0, 2.0, 3.0, 4.0, 5.0], dtype='float64', name='reltime'))
+    assert reltime.equals(pd.Index([0.0, 1.0, 2.0, 3.0, 4.0], dtype='float64', name='reltime'))
 
 
 def test_trf_SideEffects(df, df_trf):
     df_copy = df.copy()
-    df_time = rs.feature_transformers.timeindex_from_colsuffix(df)
+    df_time = rs.feature_transformers.timeindex_from_colsuffix(df).stack('ID')
     shift_dict = dict(zip(["m2", "m1", "p1"], [-1, 0, 1]))
-    t1 = rs.feature_transformers.TAFtoTRF(shift_dict)
+    t1 = rs.feature_transformers.TAFtoTRF(shift_dict, "ID")
     p1 = make_pipeline(t1)
     result = p1.fit_transform(df_time)
     assert_frame_equal(df, df_copy)
@@ -117,22 +116,12 @@ def test_trf_SideEffects(df, df_trf):
 
 def test_trf_result(df, df_trf):
     df_time = rs.feature_transformers.timeindex_from_colsuffix(df)
-    shift_dict = dict(zip(["m2", "m1", "p1"], [-1, 0, 1]))
-    t1 = rs.feature_transformers.TAFtoTRF(shift_dict)
-    p1 = make_pipeline(t1)
-    result = p1.fit_transform(df_time)
-    # integers are converted to float during shifting to allow NaN values, which is expected behaviour
-    assert_frame_equal(result, df_trf, check_dtype=False)
-
-
-def test_trf_exclusion(df, df_trf):
-    df_time = rs.feature_transformers.timeindex_from_colsuffix(df)
 
     rs.sub_routines.sort_index_columns_inplace(df_time)
     df_time = df_time.stack('ID')
 
     shift_dict = dict(zip(["m2", "m1", "p1"], [-1, 0, 1]))
-    t1 = rs.feature_transformers.TAFtoTRF(shift_dict, exclude=(slice('ID_5','ID_5'),slice('Feature_1')))
+    t1 = rs.feature_transformers.TAFtoTRF(shift_dict, 'ID')
     p1 = make_pipeline(t1)
     result = p1.fit_transform(df_time)
     # integers are converted to float during shifting to allow NaN values, which is expected behaviour
